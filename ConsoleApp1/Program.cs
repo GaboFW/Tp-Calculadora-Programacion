319ca17219e7e57549526f66455aaba6999109b7
GaboFW <gabibo.fewars@gmail.com>
2023-11-11 15:18:01 -0300

    commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperCalculator supercalculator = new SuperCalculator();
+            supercalculator += 3;
+            supercalculator += 3;
+            supercalculator += 1;
+            Console.WriteLine($"Resultado {supercalculator.Resultado}");
+            Console.ReadKey();
+        }
+    }
+
+    public class SuperCalculator
+    {
+        private Stack<int> numeros;
+
+        public SuperCalculator()
+        {
+            this.numeros = new Stack<int>();
+        }
+
+        public double Resultado
+        {
+            get
+            {
+                int resultado = 0;
+                foreach (int numeros in this.numeros)
+                {
+                    resultado += numeros;
+                }
+                return resultado;
+            }
+        }
+
+        public static SuperCalculator operator +(SuperCalculator superCalculator, int numeros)
+        {
+            foreach (int item in superCalculator.numeros)
+            {
+                numeros *= item;
+            }
+            superCalculator.numeros.Push(numeros);
+            return superCalculator;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 5;
+            Queue<double> data = new Queue<double>();
+            int i;
+
+            for (i = max; i > 0; i--)
+            {
+                data.Enqueue(i);
+            }
+
+            for (i = 0; i < max; i++)
+            {
+                Console.Write($"{data.Dequeue()}");
+            }
+        }
+    }
+}
